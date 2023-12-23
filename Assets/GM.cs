49674521faa72e3dc5b39b59ea68bb4c7a6ea3c9
RamezNasser5 <rameznasser55@gmail.com>
2023-12-23 21:42:35 +0200

    add second level

--- Assets/GM.cs
+++ Assets/GM.cs
@@ -8,16 +8,26 @@ public class GM : MonoBehaviour
     public static float totalTime = 0;
     public static float zVelAdj = 1;
     public static string lvlCompStatus = "";
+    public Transform BlockMidPit;
+    public Transform BlockNoPit;
     public float waitToLoad = 0;
     // Start is called before the first frame update
-    // void Start()
-    // {
+    void Start()
+    {
+        if (BlockNoPit != null)
+        {
+            Instantiate(BlockNoPit.gameObject, new Vector3(0, 2.17f, 42), BlockNoPit.rotation);
+            Instantiate(BlockNoPit.gameObject, new Vector3(0, 2.17f, 46), BlockNoPit.rotation);
+        }
 
-    // }
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Instantiate(BlockMidPit.gameObject, new Vector3(0, 2.17f, 50), BlockMidPit.rotation);
+        Instantiate(BlockMidPit.gameObject, new Vector3(0, 2.17f, 54), BlockMidPit.rotation);
+
         totalTime += Time.deltaTime;
         if (lvlCompStatus == "Fail")
         {
